95dce1a3262bc0fbcc44f561afd9f8f90cabfcf8
szuletett <szuletett@gmail.com>
2014-12-09 23:36:40 +0100

    finished crud for public wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -15,18 +15,47 @@ class WikisController < ApplicationController
   def create
     @wiki = Wiki.new(wiki_params)
     if @wiki.save
+      notice = "created!"
       redirect_to @wiki
     else
+      notice = "error creating"
       render 'index'
     end
   end
 
   def edit
+
+    @wiki = Wiki.find(params[:id])
+    
+  end
+
+  def update
+
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      notice = "Updated!"
+      redirect_to @wiki
+    else
+      alert = "error saving the post"
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      notice = "destroyed!"
+      redirect_to wikis_path
+    else
+      notice = "error deleting"
+      render :show
+    end
+
   end
 
   def wiki_params
     params.require(:wiki).permit(:title, :body)
   end
-
   
 end
